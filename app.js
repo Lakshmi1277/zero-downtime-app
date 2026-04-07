5b454d927591c2c39ccf9a96faf6dd905c491cc7
Adarsh <cherry1277r@gamil.com>
2026-04-08 00:35:26 +0530

    Trigger CI/CD

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ const app = express();
 app.get('/', (req, res) => {
   res.send("Version 22 running 🚀");
 });
-
+// Trigger CI/CD pipeline for zero downtime deployment demo
 app.listen(3000, () => console.log("Server running on port 3000"));
